db708d268bfb97919ff3f258fe9ccee6acdd33a8
imran <imrankhan99281@gmail.com>
2023-05-16 11:46:34 +0500

    Update the ImageUrl for the input form field

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -64,6 +64,18 @@ namespace BulkyWeb.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images\product");
 
+                    if(!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        //delete the old image
+                        var oldImagePath =
+                            Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using ( var fileStream = new FileStream(Path.Combine(productPath, fileName),FileMode.Create))
                     {
                         file.CopyTo(fileStream);
@@ -72,7 +84,15 @@ namespace BulkyWeb.Areas.Admin.Controllers
                     productVM.Product.ImageUrl = @"\images/product\" + fileName;
                 }
 
-                _UnitOfWork.Product.Add(productVM.Product);
+                if(productVM.Product.Id == 0)
+                {
+                    _UnitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _UnitOfWork.Product.Update(productVM.Product);
+                }
+               
                 _UnitOfWork.Save();
                 TempData["success"] = "Catagory Created Successfully";
                 return RedirectToAction("Index");
